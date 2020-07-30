afecc84440ad94a3295baf34eeaa83f38fdf2648
Suroor Hussain <suroorhussain@hamon.in>
2020-07-30 15:01:24 +0530

    Submit the form to create an article

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template
-from utils import load_article
+from flask import Flask, render_template, request, redirect, url_for
+from utils import load_article, save_article
 
 app = Flask(__name__)
 
@@ -12,9 +12,15 @@ def view_article(title):
     content = load_article(title)
     return render_template('article.html', title=title, content=content)
 
-@app.route('/write/')
+@app.route('/write/', methods=['GET', 'POST'])
 def write_article():
-    return render_template('write.html')
+    if request.method == 'GET':
+        return render_template('write.html')
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+        save_article(title, content)
+        return redirect(url_for('view_article', title=title))
 
 if __name__ == '__main__':
     app.run(debug=True)
